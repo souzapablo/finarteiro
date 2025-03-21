557e801a708ac934aac89261ab4cfb4b46676392
Pablo Souza <contato@souzapablo.dev.br>
2025-03-21 19:29:52 -0300

    feat: install Swashbuckle and add swagger

--- src/Finarteiro.Api/Program.cs
+++ src/Finarteiro.Api/Program.cs
@@ -1,6 +1,7 @@
 using Finarteiro.Api.Features.Customers;
 using Finarteiro.Api.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +14,34 @@ var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(configuration =>
     configuration.RegisterServicesFromAssembly(assembly));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Finarteiro API",
+        Description = "An ASP.NET Core Web API for managing a small craft bussiness",
+        Contact = new OpenApiContact
+        {
+            Name = "Pablo Souza",
+            Email = "contato@souzapablo.dev.br",
+            Url = new Uri("https://souzapablo.devd.br")
+        }
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 app.MapCustomerEndpoints();
 
