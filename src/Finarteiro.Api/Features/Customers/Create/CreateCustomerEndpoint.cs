cac984591800cacd8305e9b854483e7acd7d4d0a
Pablo Souza <contato@souzapablo.dev.br>
2025-03-22 09:13:02 -0300

    feat: add new information at create customer endpoint

--- src/Finarteiro.Api/Features/Customers/Create/CreateCustomerEndpoint.cs
+++ src/Finarteiro.Api/Features/Customers/Create/CreateCustomerEndpoint.cs
@@ -1,4 +1,5 @@
 ﻿using Finarteiro.Api.Common.Base;
+using Finarteiro.Api.Common.Result;
 using Finarteiro.Api.Extensions;
 using FluentValidation;
 using MediatR;
@@ -8,7 +9,13 @@ namespace Finarteiro.Api.Features.Customers.Create;
 public class CreateCustomerEndpoint : IEndpoint
 {
     public static void Map(IEndpointRouteBuilder app) =>
-        app.MapPost("", HandleAsync);
+        app.MapPost("", HandleAsync)
+            .WithName("Create customer")
+            .WithDescription("Create a new customer in the application")
+            .Produces<Result<Guid>>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status422UnprocessableEntity)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status500InternalServerError);
 
     public static async Task<IResult> HandleAsync(
         ISender sender,
